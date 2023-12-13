7aec7558c79e5680a96a3f4291c792756be1a552
VaibhavR88 <144153987+VaibhavR88@users.noreply.github.com>
2023-12-13 20:21:20 +0530

    Add files via upload

---
+++ heapSort.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+void heapify(int arr[],int N,int i)
+{
+    int largest = i;
+    int l = 2*i+1;
+    int r = 2*i+2;
+    if(l<N && arr[l]>arr[largest])
+    {
+        largest = l;
+    }
+    if(r<N && arr[r]>arr[largest])
+    {
+        largest = r;
+    }
+    if(largest != i)
+    {
+        swap(arr[i],arr[largest]);
+        heapify(arr,N,largest);
+    }
+}
+void heapSort(int arr[], int n)
+{
+    for(int i = (n/2)-1;i>=0;i--)
+    {
+        heapify(arr,n,i);
+    }
+    for(int i=n-1;i>0;i--)
+    {
+        swap(arr[0],arr[i]);
+        heapify(arr,i,0);
+    }
+
+}
+
+int main()
+{
+    int arr[5] = {5,3,2,1,0};
+    heapSort(arr,5);
+    for(int number : arr)
+    {
+        cout<<number<<" ";
+    }
+}
